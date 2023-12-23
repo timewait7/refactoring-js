7a78970ffd1e6f9012c6acd0408643e48c2d83a3
linfeng <1029776125@qq.com>
2023-12-23 20:37:11 +0800

    inline variables

--- firstExample.js
+++ firstExample.js
@@ -26,8 +26,6 @@ function statement(invoice, plays) {
         }).format;
 
     for (let perf of invoice.performances) {
-        let thisAmount = amountFor(perf);
-
         // add volume credits
         volumeCredits += Math.max(perf.audience - 30, 0);
         // add extra credit for every ten comedy attendees
@@ -35,8 +33,8 @@ function statement(invoice, plays) {
             volumeCredits += Math.floor(perf.audience / 5);
 
         // print line for this order
-        result += ` ${playFor(perf).type}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
-        totalAmount += thisAmount;
+        result += ` ${playFor(perf).type}: ${format(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
+        totalAmount += amountFor(perf);
     }
     result += `Amount owed is ${format(totalAmount / 100)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
