e431b6feda84b1cff347fb9f6e60ba552fbb50d0
linfeng <1029776125@qq.com>
2023-12-23 23:14:56 +0800

    move function: volumeCreditsFor()

--- firstExample.js
+++ firstExample.js
@@ -23,6 +23,7 @@ function statement(invoice, plays) {
         const result = Object.assign({}, aPerformance);
         result.play = playFor(result);
         result.amount = amountFor(result);
+        result.volumeCredits = volumeCreditsFor(result);
         return result;
     }
 
@@ -51,6 +52,14 @@ function statement(invoice, plays) {
         }
         return result;
     }
+
+    function volumeCreditsFor(aPerformance) {
+        let result = 0;
+        result += Math.max(aPerformance.audience - 30, 0);
+        if ("comedy" === aPerformance.play.type)
+            result += Math.floor(aPerformance.audience / 5);
+        return result;
+    }
 }
 
 function renderPlainText(data, plays) {
@@ -74,7 +83,7 @@ function renderPlainText(data, plays) {
     function totalVolumeCredits() {
         let result = 0;
         for (let perf of data.performances) {
-            result += volumeCreditsFor(perf);
+            result += perf.volumeCredits;
         }
         return result;
     }
@@ -87,14 +96,6 @@ function renderPlainText(data, plays) {
                 currency: "USD",
                 minimumFractionDigits: 2
             }).format(aNumber / 100);
-    }
-
-    function volumeCreditsFor(aPerformance) {
-        let result = 0;
-        result += Math.max(aPerformance.audience - 30, 0);
-        if ("comedy" === aPerformance.play.type)
-            result += Math.floor(aPerformance.audience / 5);
-        return result;
     }
 
 }
