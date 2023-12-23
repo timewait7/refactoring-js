a23459bba767eac3e255df71f7cedeb9813ede65
linfeng <1029776125@qq.com>
2023-12-23 22:08:27 +0800

    rename local variable

--- firstExample.js
+++ firstExample.js
@@ -74,11 +74,11 @@ function usd(aNumber) {
 }
 
 function totalVolumeCredits() {
-    let volumeCredits = 0;
+    let result = 0;
     for (let perf of invoice.performances) {
-        volumeCredits += volumeCreditsFor(perf);
+        result += volumeCreditsFor(perf);
     }
-    return volumeCredits;
+    return result;
 }
 
 console.log(statement(invoice, plays));
