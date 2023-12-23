3c5d6daffab1414624c2c609811e43369812453c
linfeng <1029776125@qq.com>
2023-12-23 22:13:14 +0800

    extract function&inline variable

--- firstExample.js
+++ firstExample.js
@@ -14,16 +14,12 @@ const plays = {
 };
 
 function statement(invoice, plays) {
-    let totalAmount = 0;
     let result = `Statement for ${invoice.customer}\n`;
     for (let perf of invoice.performances) {
-
-        // print line for this order
         result += ` ${playFor(perf).type}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
-        totalAmount += amountFor(perf);
     }
 
-    result += `Amount owed is ${usd(totalAmount)}\n`;
+    result += `Amount owed is ${usd(totalAmount())}\n`;
     result += `You earned ${totalVolumeCredits()} credits\n`;
     return result;
 }
@@ -77,6 +73,14 @@ function totalVolumeCredits() {
     let result = 0;
     for (let perf of invoice.performances) {
         result += volumeCreditsFor(perf);
+    }
+    return result;
+}
+
+function totalAmount() {
+    let result = 0;
+    for (let perf of invoice.performances) {
+        result += amountFor(perf);
     }
     return result;
 }
